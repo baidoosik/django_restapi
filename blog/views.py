1b949df7bef47b52235d6e117e38166ea18b1a1f
baidoosik <qoentlr37@naver.com>
2017-12-28 23:26:17 +0900

    django restframework 없이 api 응답 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, QueryDict, HttpResponse
+from .forms import PostForm
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return JsonResponse(post)
+        return JsonResponse(form.errors)
+
+    else:
+        return JsonResponse(Post.objects.all())
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'PUT':
+        put_data = QueryDict(request.body)
+        form = PostForm(put_data, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return JsonResponse(post)
+        return JsonResponse(form.errors)
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse
+    else:
+        return JsonResponse(post)
